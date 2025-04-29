f331c5bfc12cc63271bb362046406f59ecca3f64
boytur <mask30254@gmail.com>
2025-04-29 08:53:23 +0700

    add CORS settings to main application for handling cross-origin requests

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"rag-searchbot-backend/handlers"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	// CORS settings
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173", "http://192.168.1.105:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.POST("/upload", handlers.UploadHandler)
 	r.POST("/ask", handlers.AskHandler)
