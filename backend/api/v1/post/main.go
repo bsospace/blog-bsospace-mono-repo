11d04d23d10ecc0fb9f95fe0f781c3a243aa148d
boytur <mask30254@gmail.com>
2025-05-24 12:49:38 +0700

    feat: integrate user and crypto services with authentication middleware in post routes

--- backend/api/v1/post/main.go
+++ backend/api/v1/post/main.go
@@ -2,7 +2,10 @@ package post
 
 import (
 	"rag-searchbot-backend/internal/cache"
+	"rag-searchbot-backend/internal/middleware"
 	"rag-searchbot-backend/internal/post"
+	"rag-searchbot-backend/internal/user"
+	"rag-searchbot-backend/pkg/crypto"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -14,10 +17,34 @@ func RegisterRoutes(router *gin.RouterGroup, db *gorm.DB, cache *cache.Service) 
 	service := post.NewPostService(repo)
 	handler := NewPostHandler(service)
 
+	// สร้าง Repository ที่ใช้ GORM
+	userRepository := user.NewRepository(db)
+
+	// สร้าง Service ที่ใช้ Crypto
+	crypto := crypto.NewCryptoService()
+
+	// cryptoService
+	cryptoService := crypto
+	// สร้าง Service ที่ใช้ Repository
+	userService := user.NewService(userRepository)
+
+	// Cache Service
+	cacheService := cache
+
+	// auth middleware
+	authMiddleware := middleware.AuthMiddleware(userService, cryptoService, cacheService)
+
 	// Protected Category Routes
 	postsRoutes := router.Group("/posts")
 	{
 		postsRoutes.GET("", handler.GetAll)
-		postsRoutes.GET("/:slug", handler.GetBySlug)
+		// postsRoutes.GET("/:slug", handler.GetBySlug)
+	}
+
+	postsRoutes.Use(authMiddleware)
+	{
+		postsRoutes.POST("", handler.Create)
+		postsRoutes.GET("/:short_slug", handler.GetByShortSlug)
+
 	}
 }
