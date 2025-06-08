35cc6e54c4b33d585a917a7f7682f30ea3796f4a
boytur <mask30254@gmail.com>
2025-06-08 14:39:33 +0700

    feat: add GetPublicPostBySlugAndUsernameResponse DTO and mapping function

--- backend/api/v1/post/dto.go
+++ backend/api/v1/post/dto.go
@@ -1,9 +1,73 @@
 package post
 
+import (
+	"rag-searchbot-backend/internal/models"
+	"time"
+)
+
 type PostContentStructure struct {
 	Type    string                 `json:"type"`
 	Attrs   map[string]string      `json:"attrs,omitempty"`
 	Marks   []map[string]string    `json:"marks,omitempty"`
 	Text    string                 `json:"text,omitempty"`
 	Content []PostContentStructure `json:"content,omitempty"`
+}
+type GetPublicPostBySlugAndUsernameResponse struct {
+	ID          string     `json:"id"`
+	Slug        string     `json:"slug"`
+	ShortSlug   string     `json:"short_slug"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Thumbnail   string     `json:"thumbnail"`
+	Content     string     `json:"content"`
+	Published   bool       `json:"published"`
+	PublishedAt time.Time  `json:"published_at"`
+	Likes       int        `json:"likes"`
+	Views       int        `json:"views"`
+	ReadTime    int        `json:"read_time"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	DeletedAt   *time.Time `json:"deleted_at"`
+	AuthorID    string     `json:"author_id"`
+	Author      struct {
+		Avatar   string `json:"avatar"`
+		Username string `json:"username"`
+		Bio      string `json:"bio"`
+	} `json:"author"`
+}
+
+func MapGetPublicPostBySlugAndUsernameResponse(post *models.Post) *GetPublicPostBySlugAndUsernameResponse {
+	if post == nil {
+		return nil
+	}
+
+	dto := &GetPublicPostBySlugAndUsernameResponse{
+		ID:          post.ID.String(),
+		Slug:        post.Slug,
+		ShortSlug:   post.ShortSlug,
+		Title:       post.Title,
+		Description: post.Description,
+		Thumbnail:   post.Thumbnail,
+		Content:     post.Content,
+		Published:   post.Published,
+		PublishedAt: *post.PublishedAt,
+		Likes:       post.Likes,
+		Views:       post.Views,
+		ReadTime:    int(post.ReadTime),
+		CreatedAt:   post.CreatedAt,
+		UpdatedAt:   post.UpdatedAt,
+		DeletedAt:   &post.DeletedAt.Time,
+	}
+
+	dto.Author = struct {
+		Avatar   string `json:"avatar"`
+		Username string `json:"username"`
+		Bio      string `json:"bio"`
+	}{
+		Avatar:   post.Author.Avatar,
+		Username: post.Author.UserName,
+		Bio:      post.Author.Bio,
+	}
+
+	return dto
 }
