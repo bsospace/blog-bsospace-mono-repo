298d8bed55b699aac819586ce60efac409cab023
boytur <mask30254@gmail.com>
2025-06-13 21:08:12 +0700

    feat: implement UpdateUser method in UserHandler for updating user data

--- backend/api/v1/user/handler.go
+++ backend/api/v1/user/handler.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"rag-searchbot-backend/internal/user"
+	"rag-searchbot-backend/pkg/ginctx"
 	"rag-searchbot-backend/pkg/response"
 
 	"github.com/gin-gonic/gin"
@@ -38,4 +39,45 @@ func (h *UserHandler) GetExistingUsername(c *gin.Context) {
 		return
 	}
 	response.JSONSuccess(c, 200, "Success", user)
+}
+
+type UpdateUserRequest struct {
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Bio       string `json:"bio"`
+}
+
+// Update user data
+func (h *UserHandler) UpdateUser(c *gin.Context) {
+	user, ok := ginctx.GetUserFromContext(c)
+
+	if !ok || user == nil {
+		response.JSONError(c, 401, "Unauthorized", "User not found in context")
+		return
+	}
+
+	// ดึงข้อมูลที่ต้องการอัพเดตจาก request body
+	var updateData UpdateUserRequest
+
+	if err := c.ShouldBindJSON(&updateData); err != nil {
+		response.JSONError(c, 400, "Bad Request", "Invalid request data")
+		return
+	}
+
+	// สร้าง user object ใหม่จากข้อมูลที่ได้รับ
+	user.UserName = updateData.Username
+	user.FirstName = updateData.FirstName
+	user.LastName = updateData.LastName
+	user.Bio = updateData.Bio
+	updatedUser, err := h.userService.UpdateUser(user)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			response.JSONError(c, 404, "User not found", "User does not exist")
+			return
+		}
+		response.JSONError(c, 500, "Internal Server Error", err.Error())
+		return
+	}
+	response.JSONSuccess(c, 200, "User updated successfully", updatedUser)
 }
