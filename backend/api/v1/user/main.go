96c75b79765558c0fb0e3bfd5083fe574e92306d
boytur <mask30254@gmail.com>
2025-06-13 21:08:18 +0700

    feat: add route for checking existing username in user registration

--- backend/api/v1/user/main.go
+++ backend/api/v1/user/main.go
@@ -2,7 +2,9 @@ package user
 
 import (
 	"rag-searchbot-backend/internal/cache"
+	"rag-searchbot-backend/internal/middleware"
 	"rag-searchbot-backend/internal/user"
+	"rag-searchbot-backend/pkg/crypto"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -18,10 +20,25 @@ func RegisterRoutes(router *gin.RouterGroup, db *gorm.DB, cache *cache.Service) 
 	// สร้าง UserHandler
 	userHandler := NewUserHandler(userService)
 
+	// สร้าง Service ที่ใช้ Crypto
+	crypto := crypto.NewCryptoService()
+
+	// cryptoService
+	cryptoService := crypto
+	// Cache Service
+	cacheService := cache
+
+	// auth middleware
+	authMiddleware := middleware.AuthMiddleware(userService, cryptoService, cacheService)
+
 	// กำหนดเส้นทางสำหรับ User
 	userRoutes := router.Group("/user")
+
+	userRoutes.Use(authMiddleware)
 	{
 		// ตรวจสอบว่า username มีอยู่หรือไม่
 		userRoutes.GET("/check-username", userHandler.GetExistingUsername)
+		// อัพเดตข้อมูลผู้ใช้
+		userRoutes.PUT("/update", userHandler.UpdateUser)
 	}
 }
