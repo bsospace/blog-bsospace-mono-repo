3768c569dbaabe2e31c74f5542803058c20c3d4e
boytur <mask30254@gmail.com>
2025-06-14 15:00:44 +0700

    feat: ensure logs directory exists before initializing logger and update log file paths

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"log"
+	"os"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -13,18 +14,42 @@ var (
 
 // InitLogger sets up a global zap.Logger instance
 func InitLogger(env string) {
+	//  Ensure logs folder exists
+	if _, err := os.Stat("logs"); os.IsNotExist(err) {
+		if err := os.Mkdir("logs", 0755); err != nil {
+			log.Fatalf("failed to create logs folder: %v", err)
+		}
+	}
+
 	var cfg zap.Config
 
 	if env == "production" {
 		cfg = zap.NewProductionConfig()
-		cfg.OutputPaths = []string{"stdout", "logs/app.log"}
+		cfg.OutputPaths = []string{"stdout", "logs/logs.txt"}
 		cfg.ErrorOutputPaths = []string{"stderr"}
 		cfg.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+
+		cfg.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder // "INFO", "ERROR" (no color)
+		cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	} else {
 		cfg = zap.NewDevelopmentConfig()
-		cfg.EncoderConfig.TimeKey = "time"
-		cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		cfg.OutputPaths = []string{"stdout", "logs/logs.txt"}
+		cfg.ErrorOutputPaths = []string{"stderr"}
 		cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+
+		cfg.EncoderConfig = zapcore.EncoderConfig{
+			TimeKey:        "time",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			MessageKey:     "msg",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.CapitalColorLevelEncoder, // With color in dev
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.StringDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		}
 	}
 
 	var err error
