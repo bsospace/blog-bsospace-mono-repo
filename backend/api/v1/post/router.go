eed0e736ed472011a0a30d61e021dbbcd99df6c0
boytur <mask30254@gmail.com>
2025-07-11 18:18:24 +0700

    feat: refactor RegisterRoutes to integrate Asynq ServeMux for task handling

--- backend/api/v1/post/router.go
+++ backend/api/v1/post/router.go
@@ -1,32 +1,57 @@
 package post
 
 import (
+	"log"
 	"rag-searchbot-backend/internal/container"
 	"rag-searchbot-backend/internal/middleware"
 	"rag-searchbot-backend/internal/notification"
 	"rag-searchbot-backend/internal/post"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hibiken/asynq"
 )
 
-func RegisterRoutes(router *gin.RouterGroup, container *container.Container) {
-	handler := NewPostHandler(container.PostService.(*post.PostService))
+func RegisterRoutes(router *gin.RouterGroup, container *container.Container, mux *asynq.ServeMux) {
+	if container.AsynqClient == nil {
+		log.Fatal("[FATAL] AsynqClient is nil! Cannot enqueue tasks.")
+	}
+
+	// Auth Middleware
 	authMiddleware := middleware.NewAuthMiddleware(
 		container.UserService,
 		container.CryptoService,
 		container.CacheService,
 		container.Log,
 	)
+
+	// Worker Setup
 	worker := post.FilterPostWorker{
 		Logger:      container.Log,
 		PostRepo:    container.PostRepo,
 		QueueRepo:   container.QueueRepo,
 		NotiService: container.NotificationService.(*notification.NotificationService),
 	}
-	container.AsynqMux.HandleFunc(post.TaskTypeFilterPostContentByAI, post.FilterPostContentByAIWorkerHandler(worker))
+
+	// Create Service + Handler
+	taskEnqueuer := post.NewTaskEnqueuer(container.AsynqClient, container.QueueRepo)
+	postService := post.NewPostService(container.PostRepo, container.MediaService, taskEnqueuer)
+
+	mux.HandleFunc(post.TaskTypeFilterPostContentByAI, post.FilterPostContentByAIWorkerHandler(worker))
+
+	ps, ok := postService.(*post.PostService)
+	if !ok {
+		log.Fatal("[FATAL] Failed to cast postService to *post.PostService")
+	}
+	handler := NewPostHandler(ps)
+
+	// Route Grouping
 	postsRoutes := router.Group("/posts")
+
+	// Public routes
 	postsRoutes.GET("", handler.GetAll)
 	postsRoutes.GET("/public/:username/:slug", handler.GetPublicPostBySlugAndUsername)
+
+	// Protected routes
 	postsRoutes.Use(authMiddleware.Handler())
 	{
 		postsRoutes.POST("", handler.Create)
