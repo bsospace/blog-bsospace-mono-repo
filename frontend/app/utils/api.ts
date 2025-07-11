c2f245f0a3bb297384a93ee7a88aa8b3687a0d0b
boytur <mask30254@gmail.com>
2025-07-11 09:25:18 +0700

    refactor: remove access token from axios instance headers for improved security

--- frontend/app/utils/api.ts
+++ frontend/app/utils/api.ts
@@ -3,13 +3,12 @@
 import axios from 'axios';
 import envConfig from '../configs/envConfig';
 
-const getAccessToken = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null;
 // สร้าง axios instance
 export const axiosInstance = axios.create({
     baseURL: `${envConfig.apiBaseUrl}`, // กำหนด base URL
     timeout: 60000, // กำหนด timeout ในการร้องขอ (10 วินาที)
     headers: {
         'Content-Type': 'application/json', // ตั้งค่า headers เริ่มต้น
-        'Authorization': getAccessToken ? `Bearer ${getAccessToken}` : '' // ถ้ามีการใช้ token
-    }
+    },
+    withCredentials: true, // ส่งคุกกี้ไปกับคำขอ
 });
